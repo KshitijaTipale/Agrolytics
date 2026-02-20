10b43bfc0f9b6e851a70257ba1c6e65eba67eba7
Your Name <your-email@example.com>
2026-02-20 20:04:31 +0530

    feat:Factory page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FactoryDashboard from './pages/FactoryDashboard'
 import FarmerAuth from './pages/FarmerAuth'
 import ProtectedRoute from './components/ProtectedRoute'
 import FieldDetails from './pages/FieldDetails'
+import FactoryAuth from './pages/FactoryAuth'
+import FactoryProtectedRoute from './components/FactoryProtectedRoute'
 
 function App() {
   return (
@@ -35,7 +37,18 @@ function App() {
           } 
         />
         
-        <Route path="/factory" element={<FactoryDashboard />} />
+
+        {/* Factory Routes */}
+        <Route path="/factory/auth" element={<FactoryAuth />} />
+        
+        <Route 
+          path="/factory" 
+          element={
+            <FactoryProtectedRoute>
+              <FactoryDashboard />
+            </FactoryProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   )
